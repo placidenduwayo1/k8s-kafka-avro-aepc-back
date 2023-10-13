7d8d0cfd0a67eff19ed98d648a479f4593714f04
placidenduwayo1 <placide.nd@gmail.com>
2023-10-13 09:12:16 +0200

    feat(mysql-db,kafka,zookeeper,kafdrop,schema-registry,microservices-config-service, gateway-service):deploying into k8s cluster

--- Utility-Services/gateway-service/src/main/java/com/placide/k8skafkaavrogatewayservice/K8sKafkaAvroGatewayServiceApplication.java
+++ Utility-Services/gateway-service/src/main/java/com/placide/k8skafkaavrogatewayservice/K8sKafkaAvroGatewayServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -21,9 +20,4 @@ public class K8sKafkaAvroGatewayServiceApplication {
 	public DiscoveryClientRouteDefinitionLocator discoverClientRoute(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
 		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
 	}
-	@GetMapping(value = "/")
-	public String getMessage(){
-		return "Welcome to gateway service";
-	}
-
 }
