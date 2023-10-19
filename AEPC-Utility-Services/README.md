bebe222c58289fe1e13d5c0b44032bb481c12b58
placidenduwayo1 <placide.nd@gmail.com>
2023-10-19 13:30:57 +0200

    feat(): renaming utility microservices folder

---
+++ AEPC-Utility-Services/README.md
@@ -0,0 +1,5 @@
+# utility services
+This project contains utility microservices:
+- a configuration server: **microservices-config-service**
+- a registration server: **microservices-registry-service**
+- a gateway service: **gateway-service**
---
+++ AEPC-Utility-Services/gateway-service/src/main/java/com/placide/k8skafkaavrogatewayservice/K8sKafkaAvroGatewayServiceApplication.java
@@ -0,0 +1,23 @@
+package com.placide.k8skafkaavrogatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class K8sKafkaAvroGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		new SpringApplication(K8sKafkaAvroGatewayServiceApplication.class)
+				.run(args);
+	}
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator discoverClientRoute(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
+}
