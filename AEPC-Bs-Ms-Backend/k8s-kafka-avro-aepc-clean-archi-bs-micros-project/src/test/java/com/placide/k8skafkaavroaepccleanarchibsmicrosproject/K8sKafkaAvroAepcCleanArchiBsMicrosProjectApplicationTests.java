bcd4a6c3afc8ea70aa5e4cbbdecd921a081761fa
placidenduwayo1 <placide.nd@gmail.com>
2023-10-24 12:09:00 +0200

    feat(Address, Company, Employee Project, Kafka infra): defining initContainers

--- AEPC-Bs-Ms-Backend/k8s-kafka-avro-aepc-clean-archi-bs-micros-project/src/test/java/com/placide/k8skafkaavroaepccleanarchibsmicrosproject/K8sKafkaAvroAepcCleanArchiBsMicrosProjectApplicationTests.java
+++ AEPC-Bs-Ms-Backend/k8s-kafka-avro-aepc-clean-archi-bs-micros-project/src/test/java/com/placide/k8skafkaavroaepccleanarchibsmicrosproject/K8sKafkaAvroAepcCleanArchiBsMicrosProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.placide.k8skafkaavroaepccleanarchibsmicrosproject;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class K8sKafkaAvroAepcCleanArchiBsMicrosProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(this.getClass().getSimpleName());
 	}
 
 }
