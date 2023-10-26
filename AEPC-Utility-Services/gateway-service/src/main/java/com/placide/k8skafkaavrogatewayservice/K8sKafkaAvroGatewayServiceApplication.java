8f0873460e7c28bbe2ea9067ca64727056ab71b8
placidenduwayo1 <placide.nd@gmail.com>
2023-10-26 14:23:39 +0200

    feat( K8sKafkaAvroGatewayServiceApplication.java): small change

--- AEPC-Utility-Services/gateway-service/src/main/java/com/placide/k8skafkaavrogatewayservice/K8sKafkaAvroGatewayServiceApplication.java
+++ AEPC-Utility-Services/gateway-service/src/main/java/com/placide/k8skafkaavrogatewayservice/K8sKafkaAvroGatewayServiceApplication.java
@@ -6,18 +6,16 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class K8sKafkaAvroGatewayServiceApplication {
-
 	public static void main(String[] args) {
 		new SpringApplication(K8sKafkaAvroGatewayServiceApplication.class)
 				.run(args);
 	}
 	@Bean
-	public DiscoveryClientRouteDefinitionLocator discoverClientRoute(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+	public DiscoveryClientRouteDefinitionLocator discoverClientRoute(ReactiveDiscoveryClient rdc,
+																	 DiscoveryLocatorProperties dlp){
 		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
 	}
 }
